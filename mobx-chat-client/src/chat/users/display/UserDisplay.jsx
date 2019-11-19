e6c31139dc6387ff68a6022eee324e095780bbc4
Daniel Tabuenca <dtabuenc@gmail.com>
2019-11-19 07:51:00 -0600

    Blank Start

--- mobx-chat-client/src/chat/users/display/UserDisplay.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {useObserver} from "mobx-react-lite";
-
-export const UserDisplay = ({user}) => {
-    return useObserver(()=>(
-        <li>{user.displayName} {user.isTyping && <span>typing....</span>}</li>
-    ));
-};
--- mobx-chat-client/src/index.js
+++ mobx-chat-client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {ChatApplicationModel} from "./chat/ChatApplicationModel";
 import {ChatApplication} from "./chat/ChatApplication";
